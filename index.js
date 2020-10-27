55a5ec0034f17ce9be0a5e1216ec5ab4ac28d12f
ChillerDragon <chillerdragon@gmail.com>
2020-10-27 12:32:59 +0100

    Set CORS headers to allow fetch() from frontend
    All this js is new to me do I really commit my package-lock.json all the
    time?
    
    Also if I copy pasted code from two different sources I can call it my
    own special software now, right? :D

--- index.js
+++ index.js
@@ -7,6 +7,27 @@ dotenv.config()
 const db = require('./db')
 const port = 3000
 
+// Add headers
+// https://stackoverflow.com/a/18311469
+app.use(function (req, res, next) {
+
+  // TODO: make this more dynamic and decide on a front end port (9090 for now)
+  // Website you wish to allow to connect
+  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:9090');
+
+  // Request methods you wish to allow
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+  // Request headers you wish to allow
+  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+  // Set to true if you need the website to include cookies in the requests sent
+  // res.setHeader('Access-Control-Allow-Credentials', true);
+
+  // Pass to next layer of middleware
+  next();
+});
+
 app.use(bodyParser.json())
 app.use(
   bodyParser.urlencoded({
@@ -25,4 +46,3 @@ app.post('/users', db.createUser)
 app.listen(port, () => {
       console.log(`App running on port ${port}.`)
 })
-
